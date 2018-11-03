c28967ca1b04fa0889766ed764bd29083d168088
ozkan <ozkan.sari@outlook.com>
2018-11-03 13:06:06 +0300

    deneme

---
+++ kasımuc/src/main/java/com/kas/muc/medipol/kas/muc/KasımucApplication.java
@@ -0,0 +1,12 @@
+package com.kas.muc.medipol.kas.muc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KasımucApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KasımucApplication.class, args);
+	}
+}
---
+++ kasımuc/src/test/java/com/kas/muc/medipol/kas/muc/KasımucApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kas.muc.medipol.kas.muc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class KasımucApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
